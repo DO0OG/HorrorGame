6a4ba5c0070e2b9a8a88bbeaffd4b4878ee8cda3
MAD_DOGGO <tkatlrdldi@gmail.com>
2024-04-11 20:12:32 +0900

    feat: Modified A LOT
    - Manager Script Added
    - Player Script & Prefab Modified

---
+++ Shooting Horror Game/Assets/Scripts/Creatures/FieldOfView.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float radius;
+    [Range(0, 360), SerializeField] private float angle;
+
+    [SerializeField] private LayerMask targetMask;
+    [SerializeField] private LayerMask obstructionMask;
+
+    [SerializeField] public bool canSeePlayer;
+
+    const float DELAY = 0.2f;
+
+    void OnEnable()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        targetMask = LayerMask.GetMask("Player");
+
+        StartCoroutine(FOVRoutine());
+    }
+
+    void OnDisable()
+    {
+       StopAllCoroutines();
+    }
+
+    private void Update()
+    {
+        DrawFOV();
+        if (canSeePlayer)
+        {
+            Debug.Log("PLAYER");
+        }
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(DELAY);
+
+        while (true)
+        {
+            yield return wait;
+
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+                    canSeePlayer = true;
+                }
+                else
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+        }
+        else if (canSeePlayer)
+            canSeePlayer = false;
+    }
+
+    private void DrawFOV()
+    {
+        int rayCount = 50;
+        float angleStep = angle / rayCount;
+        float currentAngle = -angle / 2;
+
+        for (int i = 0; i < rayCount; i++)
+        {
+            Vector3 rayDirection = Quaternion.Euler(0, currentAngle, 0) * transform.forward;
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, rayDirection, out hit, radius, obstructionMask))
+            {
+                Debug.DrawLine(transform.position, hit.point, Color.red);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, rayDirection * radius, Color.green);
+            }
+
+            currentAngle += angleStep;
+        }
+    }
+}
---
+++ Shooting Horror Game/Assets/Scripts/Utils/Define.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Define
+{
+	public enum SceneNum
+	{
+		Consent,
+		Awake,
+		Main,
+		ARCamera,
+	}
+
+	public enum UIEvent
+	{
+		Click,
+		Pressed,
+		PointerDown,
+		PointerUp,
+	}
+
+	public enum SceneType
+	{
+		Unknown,
+		Main,
+		Navigation,
+		ARZone,
+		Street,
+		ARNavigation,
+	}
+
+	public enum Sound
+	{
+		Bgm,
+		Effect,
+		Max,
+	}
+}
